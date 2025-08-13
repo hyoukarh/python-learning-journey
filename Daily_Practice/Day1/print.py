29d840f32132eed9c487543cf51e92be83fdc163
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-13 16:41:08 +0100

    Print Function

---
+++ Daily_Practice/Day1/print.py
@@ -0,0 +1,9 @@
+print("Hello World!")  
+# A print function that displays the characters "Hello World!"
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl. \n"
+      "2. Knead the dough for 10mins. \n"
+      "3. Add 3g of Salt. \n"
+      "4. Leave to rise for 2 hours. \n"
+      "5. Bake at 200 degrees C for 30 minutes."
+)
