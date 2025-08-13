66ce3366d9c1b6fea4805bc7e25ca36b6a50cb22
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-13 16:59:24 +0100

    print mod

---
+++ Daily_Practice/Day1/print-mod.py
@@ -0,0 +1,4 @@
+print("He remebered the words said to him by his biggest fan, \"You can do this.\" \n"
+      "And he smiled to himself and said 'Yes I can.'" 
+)
+# This code uses print modifiers to out put sentnece and code without error.
