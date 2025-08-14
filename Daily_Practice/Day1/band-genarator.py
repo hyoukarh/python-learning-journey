c9f84213b391164a2932e8de0578797046ca664d
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-14 16:03:48 +0100

    Band Generator

---
+++ Daily_Practice/Day1/band-genarator.py
@@ -0,0 +1,9 @@
+print("Hi there, welcome to my Band Generator")
+# A print statement that outputs a welcome message
+
+city = input("What city are you from?\n")
+pet = input("What's the name of your pet?\n")
+# Two variables that store the users input
+
+print(city + " " + pet)
+# Outputs the value of both variables
