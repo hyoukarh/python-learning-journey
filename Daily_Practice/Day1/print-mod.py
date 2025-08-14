e8f4328c90650c739cdf3067903c5eb20ceceff4
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-14 14:53:01 +0100

    String Manipulation

--- Daily_Practice/Day1/print-mod.py
+++ Daily_Practice/Day1/print-mod.py
@@ -2,3 +2,8 @@ print("He remebered the words said to him by his biggest fan, \"You can do this.
       "And he smiled to himself and said 'Yes I can.'" 
 )
 # This code uses print modifiers to out put sentnece and code without error.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with,"+" sign')
+print('e.g. print("Hello" + "World")')
+print("New lines can be created with a backslash and n.")
