4925d69d15a814e79169e20df64bc42ec1674233
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-14 15:43:24 +0100

    Variables

---
+++ Daily_Practice/Day1/var.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+# Two variables that accept user input
+
+c = a
+a = b
+b = c
+# A third variable is created to store the first value
+
+print("a: " + a)
+print("b: " + b)
+# prints the values of the variable a and b
