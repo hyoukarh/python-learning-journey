a70d180d7d7119112aa159d2e1ccdd969f36b437
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-14 15:22:00 +0100

    Input and len fumction

---
+++ Daily_Practice/Day1/input.py
@@ -0,0 +1,8 @@
+# print("Hello " + input("What is your name?") + "!")
+# The input function takes a response from the user.
+# Then the print function prints out the string and the user input. 
+
+numberOfLetters = len(input())
+print(numberOfLetters)
+# A variable that stores the number of letters from the user input using the len function. 
+# Print then displays the data held inside the variable. 
