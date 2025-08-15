38de5e86ce3bb4403c4f4de107c2ad0748e5018c
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-15 13:25:00 +0100

    add two numbers

---
+++ Daily_Practice/Day2/Data-types.py
@@ -0,0 +1,12 @@
+two_digit_number = input()
+print(type(two_digit_number))
+# Takes input from the user
+# Checks data type of the variable
+
+digit_one = two_digit_number[0]
+digit_two = two_digit_number[1]
+# Stores the first value of the variable
+# Second vcode stores the second value of the variable
+
+print(int(digit_one) + int(digit_two))
+# Converts the variables from a string to integer data type
