d369f98a1ee22d6baf18fef84376abc9bb4ef89e
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-19 17:53:04 +0100

    Odd or Even number

---
+++ Daily_Practice/Day3/odd_even.py
@@ -0,0 +1,9 @@
+number = int(input("What number do you want to check?"))
+print(number)
+
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+# Code checks if the users input has a remainder using the modulo
+# Then prints the string that matches the conditional statement
