7ba4b9030331a42d9de0a401ab41b58267797a12
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-20 23:25:26 +0100

    ticket

---
+++ Daily_Practice/Day3/ticket.py
@@ -0,0 +1,25 @@
+print("Welcome to the Pendulum ride!\n")
+height = int(input("What's your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the Pendulum")
+    age = int(input("What's ypur age? "))
+    if age < 12:
+        bill = 5
+        print("Your ticket is $5.")
+    elif age <= 18:
+        bill = 7
+        print("Your ticket is $7.")
+    else:
+        bill = 12
+        print("Your ticket is $12.")
+
+    take_photo = input("Do you want to take a photo? Enter Y or N. ")
+    if take_photo == "Y":
+        bill += 3
+
+    print(f"your total bill is: {bill}")
+
+else:
+    print("You're no tall enough for this ride")
