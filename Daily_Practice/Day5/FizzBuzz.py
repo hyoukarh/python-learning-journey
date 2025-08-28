3e37125fea6e7371d907969b4d15020c46d35007
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-08-28 21:34:24 +0100

    FizzBuzz

---
+++ Daily_Practice/Day5/FizzBuzz.py
@@ -0,0 +1,11 @@
+input_number = int(input("Enter number: "))
+
+for number in range(0, input_number + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
