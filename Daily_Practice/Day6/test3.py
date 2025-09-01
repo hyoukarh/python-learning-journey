fe2b023c4287d9e051bcf8371dc1362b217a03b3
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-09-02 00:45:38 +0100

    maze challenge

---
+++ Daily_Practice/Day6/test3.py
@@ -0,0 +1,26 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def square():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+# Code to pass Reborg's World maze Challenge.
