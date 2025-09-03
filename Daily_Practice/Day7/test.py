ff1a91b4950bce9115b602446bcfef98a9d05eec
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-09-03 12:42:54 +0100

    hangman

--- Daily_Practice/Day7/test.py
+++ Daily_Practice/Day7/test.py
@@ -1,19 +1,79 @@
 import random
 
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 word_list = ["Bully", "Ally", "Enemy"]
 chosen_word = random.choice(word_list).lower()
+lives_left = 6
 print(chosen_word)
 
 display_underscore = ["__"] * len(chosen_word)
 
 
-while "__" in display_underscore:
+while "__" in display_underscore and lives_left > 0:
     guess_letter = input("Guess a letter: ").lower()
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess_letter:
             display_underscore[position] = letter
 
+    if guess_letter not in chosen_word:
+        lives_left -= 1
+        print(f"You have {lives_left} left.")
+        
+    print(HANGMANPICS[6 - lives_left])
     print(display_underscore)
 
-print("You win.")
+if lives_left == 0:
+    print("You lose.")
+else:
+    print("You win.")
