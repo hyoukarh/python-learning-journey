9274b1b40e83bdcfe7e501889cc831f850bea2f5
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-09-26 18:23:59 +0100

    paint checker

--- Daily_Practice/Day8/test.py
+++ Daily_Practice/Day8/test.py
@@ -11,19 +11,6 @@
 # else:
 #     print("invalid input")
 
-# import math
-
-# def paint_calc(height, width, cover):
-#     paint_cans = (height * width) / cover
-#     return paint_cans
-
-# test_h = int(input("What is the height of the wall in meters? "))
-# test_w = int(input("What is the width of the wall in meters? "))
-# coverage = 5
-
-# total_cans = paint_calc(height = test_h, width = test_w, cover = coverage)
-# print(f"You'll need {math.ceil(total_cans)} cans of paint.")
-
 def prime_checker(number):
     is_prime = True
     for i in range(2, number):
